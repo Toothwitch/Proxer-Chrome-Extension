c7d4c16bd19027e938ff49deb23efb0be9f4fd73
Toothwitch <witch@toothwit.ch>
2015-06-06 23:03:59 +0200

    Size and Color things
    Added dynamic(extension start setting) size to the popup and made it
    easier  to read by imitating the style from proxer
    also added detection for the proxer design to adapt to the users colors
    of choice
    
    (srsly need to do code cleaning there)

--- window.js
+++ window.js
@@ -1,18 +1,43 @@
 var background = chrome.extension.getBackgroundPage();
+var backgrounds = ["#5E5E5E", "#000","#F3FBFF", "#7EC9DA"];
+var colors = ["#CCCCCC", "#D8DBD6","#000", "#404040"];
 chrome.browserAction.setBadgeBackgroundColor({color: "#212121"});
 document.addEventListener('DOMContentLoaded', function() {
 	var content = "";
-	//document.getElementsByTagName("html").style.height = (screen.availHeight / 5);
-	//document.getElementsByTagName("html").style.width = (screen.availWidth / 2);
+	document.getElementsByTagName("html")[0].style.height = (screen.availHeight / 5)+ "px";
+	document.getElementsByTagName("html")[0].style.width = (screen.availWidth / 5)+ "px";
+	document.getElementsByTagName("body")[0].style.height = (screen.availHeight / 5)+ "px";
+	document.getElementsByTagName("body")[0].style.width = (screen.availWidth / 5)+ "px";
+	
 	if(background.loggedIn){
-		content = 'Benachrichtigungen:<br>';
-		content += 'AltesPN: '+background.altPN;
-		content += '<br>NeuesPN: '+background.newPN;
-		content += '<br>Freundschaftsanfragen: '+background.friends;
-		content += '<br>News: '+background.news;
-		content += '<br>Sonstiges: '+background.other;
-		content += '<br>Design: '+ background.cookie;
+		content = '<table width="100%" height="100%"><tr><td colspan="2" align="center">Benachrichtigungen:</td></tr>';
+		content += '<tr><td>AltesPN:</td><td>'+background.altPN + '</td></tr>';
+		content += '<tr><td>NeuesPN:</td><td>'+background.newPN + '</td></tr>';
+		content += '<tr><td>Freundschaftsanfragen:</td><td>'+background.friends + '</td></tr>';
+		content += '<tr><td>News:</td><td>'+background.news + '</td></tr>';
+		content += '<tr><td>Sonstiges:</td><td>'+background.other + '</td></tr></table>';
 		document.getElementById("content").innerHTML = content;
+		var style = 0;
+		switch(background.cookie){
+			case "grey":
+				style = 0;
+				break;
+			case "black":
+				style = 1;
+				break;
+			case "old_blue":
+				style = 2;
+				break;
+			case "pantsu":
+				//*flip skirt*
+				style = 3;
+				break;
+			default : 
+				style = 0;
+				break;
+		}
+		document.getElementsByTagName("html")[0].style.background = backgrounds[style];
+		document.getElementsByTagName("html")[0].style.color = colors[style];
 	}else{
 		content = '<input type="text" id="username" placeholder="Nutzername" ></input>';
 		content += '<input type="password" id="pass" placeholder="Passwort"></input>';
