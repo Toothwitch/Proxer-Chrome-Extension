be76af8487b839e19e6af9ecec0248f968fb4003
Sadeiko <Axel@axels-mbp.home>
2015-06-13 20:19:14 +0200

    Added border-bottom (nearly)

--- window.js
+++ window.js
@@ -2,6 +2,8 @@ var background = chrome.extension.getBackgroundPage();
 var backgrounds = ["#5E5E5E", "#000","#F3FBFF", "#7EC9DA"];
 var colors = ["white", "white","white", "white"];
 var bgtitle = ["#777777", "black", "#02476E", "#436D82"];
+var bordertitle = ["#777", "white", "#02476E", "#436D82"];
+
 var style = 0;
 chrome.browserAction.setBadgeBackgroundColor({color: "#212121"});
 window.onload = function() {
@@ -48,6 +50,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		content += '<tr><td>Sonstiges:</td><td>'+background.other + '</td></tr></table>';
 		document.getElementById("content").innerHTML = content;
 		document.getElementById("ueberschrift").style.background = bgtitle[style];
+		document.getElementById("ueberschrift").style.borderBottomStyle = "1px solid " + bordertitle[style];
+		
 		var links = document.getElementsByName("link");//augenkrebs...
 		for(var i=0;i< links.length; i++){
 			links[i].style.color = colors[style];
@@ -63,6 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		content = '<table width="100%" height="100%" class="wrapper"><div class="bigicon"><a href="https://www.proxer.me" target="_blank" style="color:transparent;text-decoation:none;" tabindex="-1"><img src="icon_big.png" height="28" id="pic" width="*" alt=""></a></div><tr id="ueberschrift" width="100%"><td align="center" width="100%" style="padding:1px;"><div class="margin1">Login</div></td></tr><tr class="eingabe" width="100%"><td align="center" width="100%"><div class="margin"><input type="text" id="username" placeholder="Nutzername" tabindex="1"></input><input type="password" id="pass" placeholder="Passwort" tabindex="2"></input></div></td></tr></table>';
 		document.getElementById("content").innerHTML = content;
 		document.getElementById("ueberschrift").style.background = bgtitle[style];
+		document.getElementById("ueberschrift").style.borderBottomStyle = "1px solid " + bordertitle[style];
 		
 	}
 });
